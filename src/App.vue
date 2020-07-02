e4461c709d8ec179c0cb9158876fe2101e917a1b
tomschmosby <57222062+tomschmosby@users.noreply.github.com>
2020-07-02 10:38:03 +0200

    next

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,20 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Layout></Layout>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Layout from'./submodules/base_layout/src/components/interface'
+import 
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+    Layout
   }
 }
 </script>
