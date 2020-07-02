25a7c500f240fabae15dbc963857fd56d193bb10
tomschmosby <57222062+tomschmosby@users.noreply.github.com>
2020-07-02 10:28:05 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="embed-responsive embed-responsive-16by9">
+  <iframe class="embed-responsive-item" src"URL"></iframe>
   </div>
 </template>
 
